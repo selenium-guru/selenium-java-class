a2c4384d5b9bb50cd5fe045f432332538f789b24
Ashok Tulachan <tulachanashok@gmail.com>
2018-06-19 16:33:15 -0400

    Added gitignore files along with the project

---
+++ src/SeleniumFirstClass.java
@@ -0,0 +1,16 @@
+class SeleniumFirstClass {
+    public static void main(String[] args){
+        System.out.print("Hello Students");
+        String name = "Kamal";
+        String address = "1765 Morning Star Drive, Leesburg, VA 20141";
+        int age = 32;
+        double balance = 4500.25;
+        float currency = 255.134232323f;
+        System.out.println("My name is: " + (name + 1));
+        System.out.println("My address is: " + address);
+        System.out.println("My age is: " + (age + 1));
+        System.out.println("My balance is: " + balance);
+        System.out.println("My currency value is: " + currency);
+    }
+
+}
